83580646e4cb0924841d6bd601fc9e2115e1652f
Toby Turnbull <turnbulltoby@gmail.com>
2015-12-18 01:55:58 +1100

    styling finished finally

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
 User.destroy_all
 Post.destroy_all
 Comment.destroy_all
-
-u1 = User.create( :name => "Wataru", :email => "a@a.com", :password => "a", :password_confirmation => 'a' )
-u2 = User.create( :name => "Toby", :email => "b@b.com", :password => "b", :password_confirmation => 'b' )
-u2 = User.create( :name => "Joel", :email => "c@c.com", :password => "c", :password_confirmation => 'c' )
-
-p1 = Post.create( :content => "Here is a sample post", :user_id => 1)
-p2 = Post.create( :content => "Here is a second sample post", :user_id => 2)
-p3 = Post.create( :content => "Here is a third sample post", :user_id => 3)
-
-c1 = Comment.create( :content => "Here is a sample comment", :user_id => 1)
-c2 = Comment.create( :content => "Here is a second sample comment", :user_id => 2)
-c3 = Comment.create( :content => "Here is a third sample comment", :user_id => 3)
